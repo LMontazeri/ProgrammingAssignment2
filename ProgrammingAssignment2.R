9e7de077b299e4d05f56a0a876a9acfd2010184b
LMontazeri <montazeri.leila@gmail.com>
2022-12-21 14:05:52 +0330

    projectassignment2

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()) {
+        #It creates a special "matrix" object that can cache its inverse.
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+cacheSolve <- function(x, ...) {
+        #It computes the inverse of the special matrix returned by makeCacheMatrix
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
+}
